5df7a89a7e002d9674ea99e5e51640146b9ea8a3
nikhil <nikhil@gmail.com>
2022-02-11 00:34:06 +0530

    added viewsets for list, create, retreive, update, partial_update, destroy function

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -10,42 +11,85 @@ class HelloApiView(APIView):
     """Test API View"""
     serializer_class = serializers.HelloSerializer
 
-
     def get(self, request, format=None):
         """Return a list of APIView features"""
         an_apiview = [
-                "Uses HTTP method as function(get, post, patch, put, delete)",
-                "Is similar to Django View",
-                "Gives you most control over your application logic",
-                "Is mapped manually to URLs"
+            "Uses HTTP method as function(get, post, patch, put, delete)",
+            "Is similar to Django View",
+            "Gives you most control over your application logic",
+            "Is mapped manually to URLs"
         ]
 
-        return Response({'message':'Hello!', 'an_apiview': an_apiview})
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
 
     def post(self, request):
-            """Create a hello message with our name"""
-            serializer = self.serializer_class(data=request.data)
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
 
-            if serializer.is_valid():
-                name = serializer.validated_data.get('full_name')
-                message = f'Hello {name}'
-                return Response({'message': message})
-            else:
-                return Response(
+        if serializer.is_valid():
+            name = serializer.validated_data.get('full_name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
-                )
+            )
 
-    
     def put(self, request, pk=None):
         """Handle updating an object"""
-        return Response({'method' :'PUT'})
-    
-    
+        return Response({'method': 'PUT'})
+
     def patch(self, request, pk=None):
         """Handle a partial update of an object"""
         return Response({'mathod': 'PATCH'})
-    
+
     def delete(self, request, pk=None):
         """Delete object in the database"""
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a hello message"""
+
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Route',
+            'Provides more functionality with less code'
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message':message})
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Handle retrieve an object by its ID"""
+        return Response({'http_method': 'GET'})
+
+    def update(Self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method' : 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+        return Response({'http_method': 'DELETE'})
