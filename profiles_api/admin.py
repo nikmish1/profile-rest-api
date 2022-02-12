455eca595ef5834eb377ea1db9cd453702f6ebb3
nikhil <nikhil@gmail.com>
2022-02-12 15:32:50 +0530

    Added user profile feed API

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -5,3 +5,4 @@ from profiles_api import models
 
 
 admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -20,20 +20,29 @@ class UserProfileSerializer(serializers.ModelSerializer):
             }
         }
 
-        def create(self, validated_data):
-            """Create and return a new user"""
-            user = models.UserProfile.objects.create_user(
-                email=validated_data['email'],
-                name=validated_data['name'],
-                password=validated_data['password']
-            )
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
 
-            return user
+        return user
 
-        def update(self, instance, validated_data):
-            """Handle updating user account"""
-            if 'password' in validated_data:
-                password = validated_data.pop('password')
-                instance.set_password(password)
+    def update(self, instance, validated_data):
+        """Handle updating user account"""
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
 
-            return super().update(instance, validated_data)
+        return super().update(instance, validated_data)
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
